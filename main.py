16105aaf5f30fdbf009e274bc4d6d834f0d806b6
Jacob Carse <jacob.carse@gmail.com>
2017-10-18 20:23:32 +0100

    Slight adjustments to final output

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from Active import Active
 
 
 def main():
+    accurices = []
+
     mnist = Data(balance=False)
     mnist.load_data('mnist_train.csv', 'mnist_test.csv')
 
@@ -11,12 +13,14 @@ def main():
     print('Data Size: ' + str(len(mnist.train_x)))
     model = Model(input_shape=784, num_classes=10, save_path='none', verbose=False)
     model.set_loss_params(weights=mnist.get_weights())
-    model.train(mnist.train_x, mnist.train_y, mnist.test_x, mnist.test_y)
+    accurices.append(model.train(mnist.train_x, mnist.train_y, mnist.test_x, mnist.test_y))
 
     mnist.reduce_data(percentage=0.99)
 
-    active = Active(mnist, Model(784, 10, 'none', verbose=False), 10, 0.85)
-    print(active.run(50, 500, 100))
+    active = Active(mnist, Model(784, 10, 'none', verbose=False), 10, 1.00)
+    accurices += active.run(50, 500, 100)
+
+    print(accurices)
 
 
 if __name__ == '__main__':
